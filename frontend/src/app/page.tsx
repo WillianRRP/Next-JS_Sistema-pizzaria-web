d5ea14ca90dce5ab784d7a39e86efef1ec13dad0
WillianRRP <willianrenato754@gmail.com>
2025-04-24 20:33:42 -0300

    Finishing login page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,50 @@
+import styles from './page.module.scss'
+import logoImg from '/public/logo.svg'
+import Image from 'next/image'
+import Link from 'next/link'
 
-import styles from './page.module.scss';
-export default function Home() {
-  return (
-    <main>
-      <h1 className={styles.teste}>Pagina de login</h1>
-    </main>
-  );
+export default function Page(){
+  return(
+    <>
+     <div className={styles.containerCenter}>
+        <Image
+          src="/logoG.svg" 
+          alt="Logo de la pizzería del gatito"
+          width={200} 
+          height={200}
+        />
+
+
+        <section className={styles.login}>
+          <form>
+            <input 
+              type="email"
+              required
+              name="email"
+              placeholder="Digite seu email..."
+              className={styles.input}
+            />
+
+            <input 
+              type="password"
+              required
+              name="password"
+              placeholder="***********"
+              className={styles.input}
+            />
+
+            <button type="submit" className={styles.button}>
+              Acessar
+            </button>
+          </form>
+
+          <Link href="/signup" className={styles.text}>
+            Não possui uma conta? Cadastre-se
+          </Link>
+
+        </section>
+
+      </div>      
+    </>
+  )
 }
---
+++ frontend/src/app/singup/page.tsx
@@ -0,0 +1,10 @@
+export default function singUp(){
+    return(
+        <>
+        <main>
+        <h1>Cadastro</h1>
+        </main>
+        </>
+        
+    )
+}
