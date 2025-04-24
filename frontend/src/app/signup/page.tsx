846b7aa081fc131659f4453a4c78b4bd2b6225b1
WillianRRP <willianrenato754@gmail.com>
2025-04-24 20:55:52 -0300

    Creating signup page

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,12 +1,12 @@
-import styles from './page.module.scss'
-import logoImg from '/public/logo.svg'
 import Image from 'next/image'
 import Link from 'next/link'
+import styles from '../page.module.scss'
+import logoImg from '/public/logo.svg'
 
-export default function Page(){
-  return(
-    <>
-     <div className={styles.containerCenter}>
+export default function singUp(){
+    return(
+        <>
+       <div className={styles.containerCenter}>
         <Image
           src="/logoG.svg" 
           alt="Logo de la pizzería del gatito"
@@ -16,8 +16,15 @@ export default function Page(){
 
 
         <section className={styles.login}>
-        
+        <h1>Criando sua conta</h1>
           <form>
+            <input 
+              type="text"
+              required
+              name="name"
+              placeholder="Digite seu nome..."
+              className={styles.input}
+            />
             <input 
               type="email"
               required
@@ -35,17 +42,18 @@ export default function Page(){
             />
 
             <button type="submit" className={styles.button}>
-              Acessar
+             Cadastrar
             </button>
           </form>
 
-          <Link href="/signup" className={styles.text}>
-            Não possui uma conta? Cadastre-se
+          <Link href="/" className={styles.text}>
+            Já possui uma conta? Faça login
           </Link>
 
         </section>
 
-      </div>      
-    </>
-  )
+      </div> 
+        </>
+        
+    )
 }
