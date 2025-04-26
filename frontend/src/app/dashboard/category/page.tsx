060508e9e3aa80e03299f01cbbfa10615bfd0733
WillianRRP <willianrenato754@gmail.com>
2025-04-25 22:53:04 -0300

    Categories page

---
+++ frontend/src/app/dashboard/category/page.tsx
@@ -0,0 +1,35 @@
+import styles from './styles.module.scss'
+import {Button} from '@/app/dashboard/components/button'
+
+export default function Category() {
+    async function handleRegisterCategory() {
+       'use server'
+       console.log('teste');
+    }
+
+
+  return (
+   <main className= {styles.container}>
+    <h1>Nova Categoria</h1>
+
+
+    <form 
+    className={styles.form}
+    action={handleRegisterCategory}
+    >
+        <input type='text'
+        name='name'
+        placeholder='Nome da Categoria, ex: Pizzas'
+        required
+        className={styles.input}
+        
+        />
+        <Button name='Cadastrar'/>
+
+        
+
+</form>
+
+   </main>
+  );
+}
--- frontend/src/app/dashboard/components/header/index.tsx
+++ frontend/src/app/dashboard/components/header/index.tsx
@@ -3,50 +3,41 @@
 import Link from 'next/link';
 import styles from './styles.module.scss';
 import Image from 'next/image';
-import logoImg from '/public/logoG.svg';
 import { LogOutIcon } from 'lucide-react';
 import { deleteCookie } from 'cookies-next';
 import { useRouter } from 'next/navigation';
 
-
 export function Header() {
-
   const router = useRouter();
 
   async function handleLogout() {
-    deleteCookie('session', {path: '/'});
-
-    router.replace('/');
-
+    deleteCookie('session', { path: '/' });
+    router.replace('/login');
   }
 
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
+        {/* LOGO */}
         <Link href="/dashboard">
           <Image
             src="/logoG.svg"
             alt="Logo de la pizzería del gatito"
             width={150}
             height={110}
-            priority={true}
+            priority
             quality={100}
           />
         </Link>
 
+        {/* NAVEGAÇÃO */}
         <nav>
-          <Link href='/dashboard/category'>
-          categoria
-          
-          </Link>
-          <Link href='/dashboard/products'>
-          produtos
-        
-          </Link>
+          <Link href="/dashboard/category">Categoria</Link>
+          <Link href="/dashboard/products">Produtos</Link>
 
           <form action={handleLogout}>
-            <button type='submit'>
-              <LogOutIcon size={24} color="#fff"/>
+            <button type="submit">
+              <LogOutIcon size={24} color="#fff" />
             </button>
           </form>
         </nav>
